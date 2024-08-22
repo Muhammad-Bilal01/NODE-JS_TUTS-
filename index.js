da19006db6893e0ace5845aa1981bcd63228aa60
Muhammad Bilal <mbilal254.m@gmail.com>
2024-08-23 02:08:48 +0500

    routes in nodejs

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { userSchema } from "./schema/index.js";
 import chalk from "chalk";
+import { router } from "./routes/index.js";
 
 const log = console.log;
 
@@ -19,62 +20,16 @@ app.use("/", (req, res, next) => {
 });
 
 app.get("/", (req, res) => {
-  res.send({ status: "OK", data: new Date() });
+  return res.status(200).send({ Status: 200, msg: "OK" });
 });
 
-let users = [];
+app.all("/secret", (req, res) => {
+  console.log(req.query);
 
-app.get("/users", (req, res) => {
-  try {
-    res.status(200).send({ status: 200, users: users });
-  } catch (error) {
-    console.log(error);
-  }
+  return res.send("test");
 });
 
-app.post("/user", async (req, res) => {
-  try {
-    await userSchema.validateAsync(req.body);
-    let user = { id: Date.now().toString(36), ...req.body };
-
-    users.push(user);
-
-    res
-      .status(201)
-      .send({ status: 201, user: user, message: "User add Success" });
-  } catch (error) {
-    res
-      .status(400)
-      .send({ status: 400, error: error.details || "Something went Wrong" });
-  }
-});
-
-app.delete("/user/:id", (req, res) => {
-  const { id } = req.params;
-  console.log(id);
-
-  users = users.filter((val) => val.id !== id);
-  console.log(users);
-
-  res.send({ message: "user delete Successfully with id " + id });
-});
-
-app.put("/user/:id", (req, res) => {
-  try {
-    const { id } = req.params;
-
-    const index = users.findIndex((obj) => obj.id === id);
-    //   console.log("index", index);
-
-    users.splice(index, 1, { id: id, ...req.body });
-
-    res
-      .status(201)
-      .send({ status: 201, id, message: "user update successfully!" });
-  } catch (error) {
-    res.send({ message: "Something went wrong!" });
-  }
-});
+app.use("/api", router);
 
 app.listen(PORT, () => {
   console.log("The Server in running at port:", PORT);
